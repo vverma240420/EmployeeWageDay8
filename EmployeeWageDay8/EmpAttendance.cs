07536a491e1cb1e70f70c12c722ecce8f0ed59dc
Vivek Verma <vverma240420@gmail.com>
2023-02-25 12:56:22 +0530

    For Employee Attendance

---
+++ EmployeeWageDay8/EmpAttendance.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeWageDay8
+{
+    internal class EmpAttendance
+    {
+        public static void EmployeeAttendance()
+        {
+            int ispresent = 1;
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+
+            //UC1- EmployeeAttendence
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2); //0 or 1
+            if (empAttendence == ispresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EmployeeWageDay8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageDay8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpAttendance.EmployeeAttendance();
+        }
+    }
+}
