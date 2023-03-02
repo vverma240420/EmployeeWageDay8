db6471fc3547c7da579a3f5b0854bcb246c513f7
Vivek Verma <vverma240420@gmail.com>
2023-03-02 18:34:30 +0530

    Employee Wage For Multiple Companies

---
+++ EmployeeWageDay8/EmpWageForMultipleCompanies.cs
@@ -0,0 +1,50 @@
+﻿namespace EmployeeWageDay8
+{
+    internal class EmpWageForMultipleCompanies
+    {
+
+        const int isFullTime = 1;
+        const int isPartTime = 2;
+
+        //UC8-Multiple companies
+       public static void EmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int empMontlyWage = 0;
+            int totalWorkingDays = 0;
+
+            //UC1- Employee Attendance
+            Random random = new Random();
+            //UC6-MaxHrs
+            while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
+
+            {
+                int empAttendance = random.Next(0, 3);
+                //UC3- Add Part Time Employee
+                //UC4- Switch Case
+                switch (empAttendance)
+                {
+                    case isFullTime:
+                        Console.WriteLine("Employee is FullTime");
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        Console.WriteLine("Employee is PartTime");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                totalWorkingDays++;
+                totalEmpHrs += empHrs;
+            }
+            //UC2- Employee Daily Wage
+            //UC5- Employee Monthly Wage
+            empMontlyWage = empWagePerHr * totalEmpHrs;
+            Console.WriteLine("Company : " + company);
+            Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays + ", Montly Wage of Emplyoee is " + empMontlyWage);
+        }
+    }
+}
--- EmployeeWageDay8/Program.cs
+++ EmployeeWageDay8/Program.cs
@@ -2,10 +2,19 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+    
+
+        public static void Main(string[] args)
         {
-            ComputeEmpWage.ComputeEmployeeWage();
+            Console.WriteLine("....EMP WAGE COMPUTATION....");
+
+            //UC7-Use Class Method and Variables
+            
+            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
+            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
+            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
             Console.ReadLine();
+
         }
     }
 }
