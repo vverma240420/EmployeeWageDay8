e1ac9ebf4692eb64d7c9e9612feaffd56ad274c8
Vivek Verma <vverma240420@gmail.com>
2023-03-02 18:43:45 +0530

    Adding Employee Wage For Each Companies

--- EmployeeWageDay8/EmpWageBuilder.cs
+++ EmployeeWageDay8/EmpWageBuilder.cs
@@ -1,24 +1,35 @@
 ﻿namespace EmployeeWageDay8
 {
-    internal class EmpWageForMultipleCompanies
+    internal class EmpWageBuilder
     {
 
         const int isFullTime = 1;
         const int isPartTime = 2;
 
-        //UC8-Multiple companies
-       public static void EmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
+        private string company;
+        private int empWagePerHr;
+        private int empWorkinDaysPerMonth;
+        private int empTotalWorkingHrs;
+        private int empMontlyWage;
+
+        //UC9-Total Wage for each Company
+        public EmpWageBuilder(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
+        {
+            this.company = company;
+            this.empWagePerHr = empWagePerHr;
+            this.empWorkinDaysPerMonth = empWorkinDaysPerMonth;
+            this.empTotalWorkingHrs = empTotalWorkingHrs;
+        }
+        public void EmpWage()
         {
             int empHrs = 0;
             int totalEmpHrs = 0;
-            int empMontlyWage = 0;
             int totalWorkingDays = 0;
 
             //UC1- Employee Attendance
             Random random = new Random();
             //UC6-MaxHrs
             while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
-
             {
                 int empAttendance = random.Next(0, 3);
                 //UC3- Add Part Time Employee
@@ -26,15 +37,13 @@
                 switch (empAttendance)
                 {
                     case isFullTime:
-                        Console.WriteLine("Employee is FullTime");
                         empHrs = 8;
                         break;
                     case isPartTime:
-                        Console.WriteLine("Employee is PartTime");
                         empHrs = 4;
                         break;
                     default:
-                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
                         break;
                 }
                 totalWorkingDays++;
@@ -43,8 +52,12 @@
             //UC2- Employee Daily Wage
             //UC5- Employee Monthly Wage
             empMontlyWage = empWagePerHr * totalEmpHrs;
-            Console.WriteLine("Company : " + company);
             Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays + ", Montly Wage of Emplyoee is " + empMontlyWage);
+        }
+        //UC10-ManageWageForMultipleCompanies
+        public string ToString()
+        {
+            return "Company :" + this.company + " TotalWage =" + this.empMontlyWage;
         }
     }
 }
--- EmployeeWageDay8/Program.cs
+++ EmployeeWageDay8/Program.cs
@@ -2,17 +2,28 @@
 {
     internal class Program
     {
-    
+
+
+        const int isFullTime = 1;
+        const int isPartTime = 2;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("....EMP WAGE COMPUTATION....");
 
             //UC7-Use Class Method and Variables
-            
-            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
-            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
-            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
+            //UC8-Multiple companies
+            EmpWageBuilder Tata = new EmpWageBuilder("HCL", 30, 16, 60);
+            EmpWageBuilder Google = new EmpWageBuilder("TCS", 20, 15, 80);
+            EmpWageBuilder Apple = new EmpWageBuilder("BRIDGELAB", 15, 18, 100);
+
+            Tata.EmpWage();
+            Console.WriteLine(Tata.ToString());
+            Google.EmpWage();
+            Console.WriteLine(Apple.ToString());
+            Apple.EmpWage();
+            Console.WriteLine(Google.ToString());
+
             Console.ReadLine();
 
         }
